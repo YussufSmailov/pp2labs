854c8720b182d30786b8cf556b82b6f53bb1c0c9
irresible <smailovyussuf5@gmail.com>
2025-01-19 12:52:09 +0500

    This is the correct version

--- lab1_correct.py/4_variables.py
+++ lab1_correct.py/4_variables.py
@@ -2,3 +2,34 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+#variables names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+#output variables
+x = "Python is awesome"
+print(x)
+
+#global variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+
+
