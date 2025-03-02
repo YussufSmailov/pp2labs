a63097f35b049cb425bdf0ee2a8a6f13c72fb3c1
irresible <smailovyussuf5@gmail.com>
2025-03-02 21:57:24 +0500

    final upd

--- lab6/drfls/5.py
+++ lab6/drfls/5.py
@@ -1,3 +1,9 @@
 file_name="text.txt"
 with open(file_name, "w") as file:
     file.write('[1, 2, 3, 4]')
+
+arr=[1, 2, 4]
+file_name="t.txt"
+with open(file_name, "w") as file:
+    for i in arr:
+      file.write(str(i))
